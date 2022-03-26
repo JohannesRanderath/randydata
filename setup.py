1075fef2bb15a699a8be603b2e8d6252f4bf2bc8
Johannes Randerath <joranderathsg@gmail.com>
2022-03-26 18:54:33 +0100

    fixed a pip install bug

--- setup.py
+++ setup.py
@@ -8,20 +8,15 @@ def readme():
 
 setup(
     name="randydata",
-    version="0.2",
+    version="0.3",
     description="Tools for SQL or Excel of observational data handling.",
     long_description=readme(),
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming language :: Python :: 3.8",
-        "Topic :: Data :: Excel :: SQL :: OOP"
-    ],
     keywords="excel sql data handling",
     url="https://github.com/JohannesRanderath/randydata",
     author="Johannes Randerath",
     author_email="randydata@randerath.eu",
     license="MIT",
     packages=["randydata"],
-    install_requires=["pandas", "mysql", "sqlalchemy", "IPython"],
+    install_requires=["pandas>=1.4.1", "mysql>=0.0.3", "sqlalchemy>=1.4.32", "IPython>=8.1.1"],
     include_package_data=True
 )
